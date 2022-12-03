8055f927837352afd4211025293749f4127f293f
Bambo <kyrstke@gmail.com>
2022-12-03 21:26:07 +0100

    changed 'basename' and urls to new basic viewsets

--- database/urls.py
+++ database/urls.py
@@ -18,9 +18,8 @@ router.register('groups', views.GroupViewSet, basename='groups')
 router.register('servers', views.ServerViewSet, basename='servers')
 router.register('edition_servers', views.EditionServerViewSet, basename='edition_servers')
 router.register('db_accounts', views.DBAccountViewSet, basename='db_accounts')
-router.register('basic/teacher_editions', views.SimpleTeacherEditionViewSet, basename='teacher_editions')
-router.register('basic/semesters', views.SimpleSemesterViewSet, basename='semesters')
-# router.register('add_db_account', views.AddUserAccountToExternalDB)
+router.register('basic_teacher_editions', views.SimpleTeacherEditionViewSet, basename='simple_teacher_editions')
+router.register('basic_semesters', views.SimpleSemesterViewSet, basename='simple_semesters')
 
 
 urlpatterns = [
